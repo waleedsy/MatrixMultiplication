c057aad23a93ef111054d4c10bab4248f5aaad88
waleedsy <waleedsani2013@gmail.com>
2023-01-18 13:59:50 +0200

    Print result

--- src/MatrixThread.java
+++ src/MatrixThread.java
@@ -5,32 +5,40 @@ public class MatrixThread {
     final static int WIDTH = 100;
     final static int HEIGHT = 100;
 
-    private static class WorkerThread implements Runnable
+    private static class WorkerThread extends Thread
     {
         int[][] firstMatrix;
         int[][] secondMatrix;
         int[][] finalMatrix;
+        int start;
+        int stop;
 
-        public WorkerThread(int [][] firstMatrix, int [][] secondMatrix, int [][] finalMatrix)
+        public WorkerThread(int [][] firstMatrix, int [][] secondMatrix, int start, int stop)
         {
             this.firstMatrix = firstMatrix;
             this.secondMatrix = secondMatrix;
-            this.finalMatrix = finalMatrix;
+            this.finalMatrix = new int[HEIGHT][WIDTH];
+            this.start = start;
+            this.stop = stop;
         }
 
         public void run()
         {
-           for (int i = 0; i < WIDTH; i++)
+           for (int i = start; i < stop; i++)
            {
-               for (int j = 0; j < HEIGHT; j++)
+               for (int j = 0; j < WIDTH; j++)
                {
-                   for (int k = 0; k < HEIGHT; k++)
+                   for (int k = 0; k < WIDTH; k++)
                    {
                         finalMatrix [i][j] += firstMatrix[i][j] * secondMatrix[k][j];
                    }
                }
            }
 
+        }
+
+        public int[][] getFinalMatrix() {
+            return finalMatrix;
         }
 
     }
@@ -42,27 +50,25 @@ public class MatrixThread {
 
         for (int i = 0; i < WIDTH; i++)
         {
-            for (int j = 2; j < HEIGHT; j++)
+            for (int j = 0; j < HEIGHT; j++)
             {
                 array_tot [i][j] = rand.nextInt();
             }
         }
 
         return array_tot;
-
     }
 
     public static void main (String [] args) throws InterruptedException
     {
-        Thread[] t = new Thread[10];
+        WorkerThread[] t = new WorkerThread[10];
         int [][] fistMatrix = BigMatrix();
         int [][] secondMatrix = BigMatrix();
         int [][] finalMatrix = new int [WIDTH] [HEIGHT];
 
        for (int i = 0; i < 10; i++)
        {
-           WorkerThread threads = new WorkerThread(fistMatrix, secondMatrix, finalMatrix);
-           t[i] = new Thread(threads);
+           t[i] = new WorkerThread(fistMatrix, secondMatrix, i * 10, i * 10 + 10);
            t[i].start();
        }
 
@@ -71,11 +77,20 @@ public class MatrixThread {
            t[i].join();
        }
 
+        for (int i = 0; i < 10; i++)
+        {
+            for(int j = i * 10; j < i * 10 + 10; j++) {
+                for (int k = 0; k < WIDTH; k++) {
+                    finalMatrix[j][k] = t[i].getFinalMatrix()[j][k];
+                }
+            }
+        }
+
        for (int i = 0; i  < 100; i++)
        {
            for (int j = 0; j < 100; j++)
            {
-               System.out.print(finalMatrix[i][j] + " \n");
+               System.out.print(finalMatrix[i][j] + " ");
            }
            System.out.println(" ");
        }
